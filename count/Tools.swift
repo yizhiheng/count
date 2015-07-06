7cd2a5e6b60442e5ee8416cc1b30f75a5d957fb9
LEO <120039015@qq.com>
2015-07-05 20:37:35 -0400

    完成了逻辑,调整顺序时候有bug

---
+++ count/Tools.swift
@@ -0,0 +1,22 @@
+//
+//  Tools.swift
+//  count
+//
+//  Created by Zhiheng Yi on 2015-07-05.
+//  Copyright (c) 2015 Zhiheng Yi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Tools {
+
+    func addShadow(buttons: UIButton...) {
+        for button in buttons {
+            button.layer.shadowOffset = CGSizeMake(3, 3)
+            button.layer.shadowRadius = 3
+            button.layer.shadowColor = UIColor.grayColor().CGColor
+            button.layer.shadowOpacity = 0.5
+        }
+    }
+}
--- count/ViewController.swift
+++ count/ViewController.swift
@@ -8,57 +8,156 @@
 
 import UIKit
 import CoreData
-class ViewController: UIViewController {
+enum Flag {
+    case add
+    case minus
+}
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
+    @IBOutlet weak var newTaskButton: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+    
+    let fetchRequest = NSFetchRequest(entityName:"Task")
     let managedObjectContext = (UIApplication.sharedApplication().delegate as! AppDelegate).managedObjectContext!
+    
     var tasks = [Task]()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    
+    override func viewWillAppear(animated: Bool) {
         refresh()
     }
-
+    override func viewDidLoad() {
+        
+        newTaskButton.layer.shadowOffset = CGSizeMake(3, 3)
+        newTaskButton.layer.shadowRadius = 3
+        newTaskButton.layer.shadowColor = UIColor.grayColor().CGColor
+        newTaskButton.layer.shadowOpacity = 0.5
+        
+        
+        super.viewDidLoad()
+        self.navigationItem.leftBarButtonItem = self.editButtonItem()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         
     }
-
-    @IBAction func viewTapped(sender: AnyObject) {
-        refresh()
-        for task in tasks {
-            println("Index: \(task.index)   Content: \(task.content)    Count: \(task.count)")
+    
+    override func setEditing(editing: Bool, animated: Bool) {
+        if (!tableView.editing) {
+            // Execute tasks for editing status
+            self.tableView.setEditing(true, animated: true)
+        } else {
+            // Execute tasks for non-editing status.
+            self.tableView.setEditing(false, animated: true)
         }
-        println("----------------------------------")
-
-    }
-
-    @IBAction func addTapped(sender: AnyObject) {
         
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Potentially incomplete method implementation.
+        // Return the number of sections.
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        return tasks.count
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("task", forIndexPath: indexPath) as! TableViewCell
+        cell.addButton.tag = indexPath.row
+        cell.minusButton.tag = indexPath.row
+        cell.addButton.addTarget(self, action: "addTapped:", forControlEvents: .TouchUpInside)
+        cell.minusButton.addTarget(self, action: "minusTapped:", forControlEvents: .TouchUpInside)
+        cell.content = tasks[indexPath.row].content
+        cell.count = Int(tasks[indexPath.row].count)
+        return cell
+    }
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            println("delete at \(indexPath.row)")
+            deleteTaskAtIndex(indexPath.row)
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }
+    }
+    func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+        changeIndex(tasks[fromIndexPath.row], task2: tasks[toIndexPath.row])
+        refresh()
+        tableView.reloadData()
+        
+    }
+    func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the item to be re-orderable.
+        return true
+    }
+    
+    func addTapped (sender: UIButton) {
+        updateStoredItem(tasks[sender.tag], flag: Flag.add)
+        self.tableView.reloadData()
+    }
+    func minusTapped (sender: UIButton) {
+        updateStoredItem(tasks[sender.tag], flag: Flag.minus)
+        self.tableView.reloadData()
+    }
+    
+    func updateStoredItem (task: Task, flag: Flag) {
+        switch flag {
+        case .add: task.count += 1
+        case .minus:
+            if task.count == 0 {
+                task.count = 0
+            } else {
+                task.count -= 1
+            }
+        default: ()
+        }
+        save()
+    }
+    
+    func changeIndex (task1: Task, task2: Task) {
+        let medium = task1.index
+        task1.index = task2.index
+        task2.index = medium
+        save()
+    }
+    func deleteTaskAtIndex (index: Int) {
+        managedObjectContext.deleteObject(tasks[index])
+        tasks.removeAtIndex(index)
+        for task in tasks {
+            if task.index > Int16(index) {
+                task.index -= 1
+            }
+        }
+        save()
+    }
+    func addNewTask (content: String) {
         let entity =  NSEntityDescription.entityForName("Task", inManagedObjectContext: managedObjectContext)
         let task = Task(entity: entity!, insertIntoManagedObjectContext: managedObjectContext)
         task.index = Int16(tasks.count)
-        task.content = "test"
-        task.count = Int16(arc4random_uniform(10))
+        task.content = content
+        task.count = Int16(0)
         save()
         refresh()
-
+        tableView.reloadData()
     }
     
-    @IBAction func deleteLastTapped(sender: AnyObject) {
-        if let last = tasks.last {
-            managedObjectContext.deleteObject(tasks.last!)
-            save()
-            refresh()
-        }
-    }
-    
-    func refresh () {
-        let fetchRequest = NSFetchRequest(entityName:"Task")
+    func refresh () -> Bool {
         let sortDescriptor = NSSortDescriptor(key: "index", ascending: true)
         fetchRequest.sortDescriptors = [sortDescriptor]
         var error: NSError?
-        if let fetchResults = managedObjectContext.executeFetchRequest(fetchRequest, error: nil) as? [Task] {
-            tasks = fetchResults
+        tasks = managedObjectContext.executeFetchRequest(fetchRequest, error: nil) as! [Task]
+        if error == nil {
+            return true
+        } else {
+            println("error")
+            return false
         }
     }
     func save () {
@@ -66,6 +165,34 @@ class ViewController: UIViewController {
         if !managedObjectContext.save(&error) {
             println("Could not save \(error), \(error?.userInfo)")
         }
+    }
+    
+    @IBAction func newTaskButtonTapped(sender: UIButton) {
+        var alertController:UIAlertController?
+        alertController = UIAlertController(title: "Enter Text", message: "Enter some text below", preferredStyle: .Alert)
+        alertController!.addTextFieldWithConfigurationHandler({(textField: UITextField!) in
+            textField.placeholder = "Enter something"
+        })
+        let submitAction = UIAlertAction(title: "Submit",
+            style: UIAlertActionStyle.Default,
+            handler: {(paramAction:UIAlertAction!) in
+                if let textFields = alertController?.textFields {
+                    let theTextFields = textFields as! [UITextField]
+                    let enteredText = theTextFields[0].text
+                    if enteredText != "" {
+                        self.addNewTask(enteredText)
+                    }
+                }
+            }
+        )
+        
+        let cancelAction: UIAlertAction = UIAlertAction(title: "Cancel", style: .Cancel) { action -> Void in
+            //Do nothing
+        }
+        alertController?.addAction(submitAction)
+        alertController?.addAction(cancelAction)
+        
+        self.presentViewController(alertController!, animated: true, completion: nil)
     }
     
 }
