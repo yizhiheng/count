7c280bfcd1ae6482f4ce1dcf3e678c3587e480a4
LEO <120039015@qq.com>
2015-08-03 17:30:26 -0400

    add a lot features

--- count/TaskDetailViewController.swift
+++ count/TaskDetailViewController.swift
@@ -17,7 +17,9 @@ class TaskDetailViewController: UIViewController {
     @IBOutlet weak var startingNumberLabel: UILabel!
     @IBOutlet weak var stepDistanceLabel: UILabel!
     @IBOutlet weak var startingNumberIndicateLabel: UILabel!
+    @IBOutlet weak var colorBarView: UIView!
     
+    @IBOutlet weak var secondContentLabel: UILabel!
     @IBOutlet weak var settingStatusView: UIView!
     @IBOutlet weak var nameStatusView: UIView!
     @IBOutlet weak var startingStatusView: UIView!
@@ -25,22 +27,32 @@ class TaskDetailViewController: UIViewController {
     @IBOutlet weak var iconStatusView: UIView!
    
     
-
     @IBOutlet weak var scrollView: UIScrollView!
-    
+
     var taskIndex: Int? {
         didSet {
         }
     }
     
     override func viewWillAppear(animated: Bool) {
-        //self.navigationController!.navigationBar.tintColor = UIColor.whiteColor()
-        //self.view.backgroundColor = bgColors[Int(tasks[tappedTaskIndex].bgColor)]
         self.view.backgroundColor = UIColor.whiteColor()
-
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let nameViewTap = UITapGestureRecognizer(target: self, action: "nameViewTapped:")
+        nameStatusView.addGestureRecognizer(nameViewTap)
+        
+        let startingViewTap = UITapGestureRecognizer(target: self, action: "startingViewTapped:")
+        startingStatusView.addGestureRecognizer(startingViewTap)
+        
+        let stepViewTap = UITapGestureRecognizer(target: self, action: "stepViewTapped:")
+        stepStatusView.addGestureRecognizer(stepViewTap)
+        
+        let iconViewTap = UITapGestureRecognizer(target: self, action: "iconViewTapped:")
+        iconStatusView.addGestureRecognizer(iconViewTap)
+        
+        
         
         settingStatusView.layer.addSublayer(upperBorder(settingStatusView))
         let statusViews = [nameStatusView, startingStatusView, stepStatusView, iconStatusView]
@@ -53,8 +65,6 @@ class TaskDetailViewController: UIViewController {
         
         countLabel.adjustsFontSizeToFitWidth = true
         
-        //self.navigationController.navigationBar.barStyle = UIBarStyleBlack
-        //
         minusButton.backgroundColor = UIColor.clearColor()
         minusButton.layer.cornerRadius = 25
         minusButton.layer.borderWidth = 0.3
@@ -64,29 +74,23 @@ class TaskDetailViewController: UIViewController {
         addButton.layer.cornerRadius = 25
         addButton.layer.borderWidth = 0.3
         addButton.layer.borderColor = UIColor.lightGrayColor().CGColor
-        //UIApplication.sharedApplication().setStatusBarStyle(UIStatusBarStyle.LightContent, animated: true)
+        
         println("Show Detail of task: \(tappedTaskIndex)")
 
-        setScrollView()
         showData()
+        setScrollView()
         
         
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
-    
-    
     func showData () {
+        colorBarView.backgroundColor = bgColors[Int(tasks[tappedTaskIndex].bgColor)]
         countLabel.text = "\(tasks[tappedTaskIndex].count)"
         contentLabel.text = "\(tasks[tappedTaskIndex].content)..."
+        secondContentLabel.text = "\(tasks[tappedTaskIndex].content)"
         startingNumberLabel.text = "\(tasks[tappedTaskIndex].startingNumber)"
         stepDistanceLabel.text = "\(tasks[tappedTaskIndex].stepDistance)"
         taskIcon.image = UIImage(named: tasks[tappedTaskIndex].icon)
-        
     }
-    
     func setScrollView () {
         
         let iconNameArray =  ["shop", "anchor", "booklet", "caution", "cruise", "gamecontroller", "hourglass", "paintroller", "rainbow", "spaceshuttle", "tractor", "art", "briefcase", "chat", "denied", "gas", "lightbulb", "parachute", "recycle", "stack", "travelerbag", "bike", "brightness", "check", "fashion", "hazard", "megaphone2", "phone", "ribbon", "star", "ufo", "blimp", "browser", "compass", "flame", "heart", "merge", "plane", "rocket", "submarine", "unlocked", "bolt", "car", "compose", "flash", "helicopter", "microphone", "present", "running", "support", "windy", "bomb", "cart", "countdown", "flower", "hotair", "motorcycle", "racingflags", "tools", "x"]
@@ -132,37 +136,45 @@ class TaskDetailViewController: UIViewController {
         taskIcon.animate()
     }
     
-    @IBAction func addTapped(sender: UIButton) {
-        updateStoredItem(tasks[tappedTaskIndex], Flag.add)
-        countLabel.animation = "pop"
-        countLabel.duration = 0.5
-        countLabel.animate()
-        showData()
+    func nameViewTapped (sender: AnyObject) {
+        let alert = SCLAlertView()
+        let nameTextField = alert.addTextField(title:"new task name...")
+        alert.addButton("Submit") {
+            alert.isGoingToDismiss = true
+            var stateChanged = false
+            if nameTextField.text != "" {
+                tasks[tappedTaskIndex].content = nameTextField.text
+                stateChanged = true
+            } else {
+                alert.isGoingToDismiss = false
+                alert.viewText.text = "Oops! Task name cannot be empty. Please try again."
+                alert.viewText.textColor = UIColor.redColor()
+                alert.viewText.animation = "shake"
+                alert.viewText.curve = "spring"
+                alert.viewText.duration = 1.0
+                alert.viewText.animate()
+            }
+            if stateChanged {
+                save()
+                self.showData()
+            }
+        }
+        alert.showEdit("Change Task Name...", subTitle:"What is the new name of this task?")
+        
     }
-    @IBAction func minusTapped(sender: UIButton) {
-        updateStoredItem(tasks[tappedTaskIndex], Flag.minus)
-        countLabel.animation = "pop"
-        countLabel.duration = 0.5
-        countLabel.animate()
-        showData()
-    }
-
-    @IBAction func settingButtonTapped(sender: UIBarButtonItem) {
+    func startingViewTapped (sender: AnyObject) {
         let alert = SCLAlertView()
         let startingNumber = alert.addTextField(title:"new starting number...")
-        let stepDistance = alert.addTextField(title:"new step distance...")
         startingNumber.keyboardType = UIKeyboardType.NumberPad
-        stepDistance.keyboardType = UIKeyboardType.NumberPad
-        
         alert.addButton("Submit") {
             alert.isGoingToDismiss = true
             var stateChanged = false
             if startingNumber.text != "" {
                 if fitInt32(startingNumber.text) {
-                        tasks[tappedTaskIndex].startingNumber = Int32(startingNumber.text.toInt()!)
-                        tasks[tappedTaskIndex].count = tasks[tappedTaskIndex].startingNumber
-                        stateChanged = true
-
+                    tasks[tappedTaskIndex].startingNumber = Int32(startingNumber.text.toInt()!)
+                    tasks[tappedTaskIndex].count = tasks[tappedTaskIndex].startingNumber
+                    stateChanged = true
+                    
                 } else {
                     alert.isGoingToDismiss = false
                     alert.viewText.text = "Oops! Starting # goes wrong. Please try again."
@@ -173,6 +185,20 @@ class TaskDetailViewController: UIViewController {
                     alert.viewText.animate()
                 }
             }
+            if stateChanged {
+                save()
+                self.showData()
+            }
+        }
+        alert.showEdit("Change starting #...", subTitle:"What is the new starting # of this task? Old record will be earsed")
+    }
+    func stepViewTapped (sender: AnyObject) {
+        let alert = SCLAlertView()
+        let stepDistance = alert.addTextField(title:"new step distance...")
+        stepDistance.keyboardType = UIKeyboardType.NumberPad
+        alert.addButton("Submit") {
+            alert.isGoingToDismiss = true
+            var stateChanged = false
             if stepDistance.text != "" {
                 if fitInt32(stepDistance.text) {
                     tasks[tappedTaskIndex].stepDistance = Int32(stepDistance.text.toInt()!)
@@ -186,17 +212,33 @@ class TaskDetailViewController: UIViewController {
                     alert.viewText.duration = 1.0
                     alert.viewText.animate()
                 }
-            
             }
             if stateChanged {
                 save()
                 self.showData()
             }
-            
         }
-        alert.showEdit("Change numbers...", subTitle:"Which # you want to modify?")
+        alert.showEdit("Change step distance...", subTitle:"What is the new step distance of this task?")
     }
-
+    func iconViewTapped (sender: AnyObject) {
+        
+        println(sender)
+        
+    }
     
+    @IBAction func addTapped(sender: UIButton) {
+        updateStoredItem(tasks[tappedTaskIndex], Flag.add)
+        countLabel.animation = "pop"
+        countLabel.duration = 0.5
+        countLabel.animate()
+        showData()
+    }
+    @IBAction func minusTapped(sender: UIButton) {
+        updateStoredItem(tasks[tappedTaskIndex], Flag.minus)
+        countLabel.animation = "pop"
+        countLabel.duration = 0.5
+        countLabel.animate()
+        showData()
+    }    
 
 }
